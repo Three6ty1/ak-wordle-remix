6475c417a7d181b3f083f2f6a3991119288ab2bb
Three6ty1 <wk.zhang02@gmail.com>
2023-12-12 16:04:43 +1100

    Added tailwind styles and font

--- ak-wordle-app/tailwind.config.ts
+++ ak-wordle-app/tailwind.config.ts
@@ -3,7 +3,19 @@ import type { Config } from 'tailwindcss'
 export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Azbuka W01 Condensed', 'Ariel'],
+      },
+      colors: {
+        incorrect: '#919090',
+        higher: '#08aadd',
+        lower: '#c80000',
+        half: '#d3470b',
+        correct: '#424142',
+        bg_main: '#edeeee',
+      },
+    },
   },
   plugins: [],
 } satisfies Config
