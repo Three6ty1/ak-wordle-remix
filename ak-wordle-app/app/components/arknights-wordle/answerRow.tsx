1e2e62bc64578249deb971df099b5710d7d1fa07
Three6ty1 <wk.zhang02@gmail.com>
2023-12-19 23:50:12 +1100

    Added @apply directive that doesnt work. will fix later

--- ak-wordle-app/app/components/arknights-wordle/answerRow.tsx
+++ ak-wordle-app/app/components/arknights-wordle/answerRow.tsx
@@ -6,20 +6,11 @@ import { getOperatorIconUrl } from "~/helper/helper";
 export default function AnswerRow(props: { guess: GuessResult, index: number }) {
     let guess: any = props.guess;
 
-    // <p>{`${guess?.name} 
-    // ${guess?.gender?.result} 
-    // ${guess?.race?.result} 
-    // ${guess?.allegiance?.result} 
-    // ${guess?.profession?.result} 
-    // ${guess?.cost?.result} 
-    // ${guess?.rarity?.result} 
-    // ${guess?.infected?.result}`}</p>
-
     const isGuesses = localStorage.getItem('guesses');
     const guesses = (isGuesses) ? JSON.parse(isGuesses) : [];
     const op = guesses[props.index]
     const url = getOperatorIconUrl(op['charId'], op['rarity'].guess)
-
+    
     delete guess['charId']
     delete guess['correct']
 
@@ -31,7 +22,7 @@ export default function AnswerRow(props: { guess: GuessResult, index: number }) 
                     ? 
                         <AnswerBox key={key} category={key} guess={guess[key as keyof typeof guess].guess} result={guess[key as keyof typeof guess].result} index={index}/>
                     : 
-                        <div key={key} className='tooltip flex mx-2 my-1 h-20 w-20 p-1 break-all items-center justify-end bg-bg_main' data-tip={guess?.name}>
+                        <div key={key} className='tooltip flex mx-2 my-1 h-20 w-20 p-1 break-all items-center justify-end bg-bg_main animate-flip' data-tip={guess?.name}>
                             <img src={url} />
                         </div>             
                 ))
